1893bb1efe1ab71a4471d313b6c82e54d94f5108
Margiett Gil <mxg627@icloud.com>
2020-08-24 10:24:37 -0400

    Objectives
    Nesting groups using compositional layout.
    Using orthogonal scrolling within sections.
    Adding header views to sections using NSCollectionLayoutBoundarySupplementaryItem.
    Be familiar with the types of orthogonal scrolling.

---
+++ Nested-Group-August-24/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nested-Group-August-24
+//
+//  Created by Margiett Gil on 8/24/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nested-Group-August-24Tests/Nested_Group_August_24Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Nested_Group_August_24Tests.swift
+//  Nested-Group-August-24Tests
+//
+//  Created by Margiett Gil on 8/24/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import XCTest
+@testable import Nested_Group_August_24
+
+class Nested_Group_August_24Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
